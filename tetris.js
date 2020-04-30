e24d0d756dd57e76c202b0a006dee6516b9f89be
adoboshi <61082185+adoboshi@users.noreply.github.com>
2020-05-01 01:24:09 +0900

    update tetris (move)

--- tetris.js
+++ tetris.js
@@ -6,15 +6,33 @@ const FIELD_COL = 10;
 const SCREEN_HEIGHT = BLOCK_SIZE * FIELD_ROW;
 const SCREEN_WIDTH = BLOCK_SIZE * FIELD_COL;
 
+const key = {
+	left: 0,
+	right: 1,
+	up: 2,
+	down: 3,
+	space: 4
+};
+let isPressed = new Array(5).fill(false);
 let can = document.getElementById("can");
 let con = can.getContext("2d");
 let field = new Array(FIELD_ROW);
+let frameCount = 0;
+let startTime;
 let mino = [
 	[0, 0, 0, 0],
 	[1, 1, 0, 0],
 	[0, 1, 1, 0],
 	[0, 0, 0, 0]
-]
+];
+class Block {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+let block = new Block(FIELD_COL / 2 - MINO_SIZE / 2, 0);
+
 function init() {
 	can.height = SCREEN_HEIGHT;
 	can.width = SCREEN_WIDTH;
@@ -24,71 +42,60 @@ function init() {
 	}
 }
 
-
+function drawBlock(x, y, color) {
+	let blockColor;
+	let lineColor;
+	switch (color) {
+		case 0:
+			blockColor = "white";
+			lineColor = "gray";
+			break;
+		default:
+			blockColor = "red";
+			lineColor = "black";
+			break;
+	}
+	let px = x * BLOCK_SIZE;
+	let py = y * BLOCK_SIZE;
+	con.fillStyle = blockColor;
+	con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+	con.strokeStyle = lineColor;
+	con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+}
 function drawField() {
 	for (let y = 0; y < FIELD_ROW; y++) {
 		for (let x = 0; x < FIELD_COL; x++) {
-			let blockColor;
-			let lineColor;
-			switch (field[y][x]) {
-				case 0:
-					blockColor = "white";
-					lineColor = "gray";
-					break;
-				default:
-					blockColor = "blue";
-					lineColor = "black";
-					break;
-			}
-			let px = x * BLOCK_SIZE;
-			let py = y * BLOCK_SIZE;
-			con.fillStyle = blockColor;
-			con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-			con.strokeStyle = lineColor;
-			con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+			drawBlock(x, y);
 		}
 	}
 }
 
-function drawBlock() {
-	for (let y = 0; y < MINO_SIZE; y++) {
-		for (let x = 0; x < MINO_SIZE; x++) {
-			if (mino[y][x]) {
-				let px = x * BLOCK_SIZE;
-				let py = y * BLOCK_SIZE;
-				con.fillStyle = "red";
-				con.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-				con.strokeStyle = "black";
-				con.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-			}
-		}
-	}
-}
 
 function update() {
-	if (isLeftPress) {
-		console.log("Left");
-	}
-	if (isRightPress) {
-
-	}
+	if(isPressed[key.left]) block.x--;
+	if(isPressed[key.right]) block.x++;
+	if(isPressed[key.up]) block.y--;
+	if(isPressed[key.down]) block.y++;
 }
 
 function draw() {
-	con.fillStyle = "white";
-	con.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
+	con.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	for (let y = 0; y < FIELD_ROW; y++) {
+		for (let x = 0; x < FIELD_COL; x++) {
+			drawBlock(x, y, field[y][x]);
+		}
+	}
+	for (let y = 0; y < MINO_SIZE; y++) {
+		for (let x = 0; x < MINO_SIZE; x++) {
+			drawBlock(block.x + x, block.y + y, mino[y][x]);
+		}
+	}
 	con.font = "20px sans-serif";
 	con.fillStyle = "black";
-	con.fillText(`frame: ${frameCount}`, 10, SCREEN_HEIGHT / 2);
-	drawField();
-	drawBlock();
+	con.fillText(`frame: ${frameCount}`, 10, 20);
 }
 
-let isLeftPress = false;
-let isRightPress = false;
-let frameCount = 0;
-let startTime;
+
 window.onload = function () {
 	this.init();
 	startTime = performance.now();
@@ -111,11 +118,41 @@ function mainLoop() {
 }
 
 document.onkeydown = function (e) {
-	if (e.keyCode === 37) isLeftPress = true;
-	if (e.keyCode === 39) isRightPress = true;
+	switch (e.keyCode) {
+		case 37: // 左
+			isPressed[key.left] = true;
+			break;
+		case 38: // 上
+			isPressed[key.up] = true;
+			break;
+		case 39: // 右
+			isPressed[key.right] = true;
+			break;
+		case 40: // 下
+			isPressed[key.down] = true;
+			break;
+		case 32: // スペース
+			isPressed[key.space] = true;
+			break;
+	}
 }
 
 document.onkeyup = function (e) {
-	if (e.keyCode === 37) isLeftPress = false;
-	if (e.keyCode === 39) isRightPress = false;
+	switch (e.keyCode) {
+		case 37: // 左
+			isPressed[key.left] = false;
+			break;
+		case 38: // 上
+			isPressed[key.up] = false;
+			break;
+		case 39: // 右
+			isPressed[key.right] = false;
+			break;
+		case 40: // 下
+			isPressed[key.down] = false;
+			break;
+		case 32: // スペース
+			isPressed[key.space] = false;
+			break;
+	}
 }
